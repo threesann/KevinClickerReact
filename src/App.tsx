11dc62dedabe5b937b410ebd5af2c9652135244e
rich abs <rishi_sandhu@outlook.com>
2024-06-18 13:27:07 +0000

    Full Release

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,10 @@ import UpgradeSection from './sections/UpgradeSection'
 
 export default function App() {
   return (
-    <main className='grid lg:grid-cols-3 lg:grid-rows-[1fr_2rem] lg:h-screen min-h-screen h-full p-6 gap-6'>
+    <main className='grid lg:grid-cols-3 lg:grid-rows-[0.5rem_1fr_2rem] lg:h-screen min-h-screen h-full p-6 gap-6'>
+      <div className='lg:col-span-3 w-full'>
+        <a href="https://threesann.github.io" className='uppercase px-2 py-1 bg-black/25 hover:bg-black/50'>Go back</a>
+      </div>
       <div className='flex flex-col lg:col-span-2 min-h-[48rem] gap-6'>
         <ClickerSection />
         <LevelSection />
